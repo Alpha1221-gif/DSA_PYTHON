a71091b39016a2830d42dd8292b2f995cee50e59
Alpha1221-gif <ishan21780@gmail.com>
2026-08-14 16:25:25 +0530

    linked list

---
+++ LINEAR_DATA_STRUCTURE/LINKED_LIST/02_Singly_linked.py
@@ -0,0 +1,35 @@
+# ----CODE1---- #
+# Making a Single linked list.
+
+class node:
+    def __init__(self,info,next=None):
+        self.info = info
+        self.next = next
+
+class SinglyLinkedList:
+    def __init__(self,head=None):
+        self.head = head
+
+# Insertion at the end
+    def at_end(self,value):
+        temp = node(value)
+        if(self.head != None):
+            t1 = self.head
+            while(t1.next != None):
+              t1 = t1.next
+            t1.next = temp
+        else:
+            self.head = temp
+
+    def printLL(self):
+        t1 = self.head
+        while t1 is not None:  
+            print(t1.info)     
+            t1 = t1.next  
+        
+
+obj = SinglyLinkedList()
+obj.at_end(10)
+obj.at_end(20)
+obj.at_end(30)
+obj.printLL()
