6e8815973e34ce6020c72f1260e95bee0dd2c659
Alpha1221-gif <ishan21780@gmail.com>
2026-09-08 21:45:41 +0530

    connit

--- LINEAR_DATA_STRUCTURE/LINKED_LIST/03_Doubly_linked.py
+++ LINEAR_DATA_STRUCTURE/LINKED_LIST/03_Doubly_linked.py
@@ -66,4 +66,101 @@ def is_mid(self,value,x):
 # Delete the Linked List
 def deletell(self,value):
     if self.head is None:
-        print("List is emp")
+        print("List is empty")
+        return
+    
+    if self.head.info == value:
+        self.head == self.head.next
+        if self.head is None:
+            self.head.prev = None
+        return
+
+    t1 = self.head
+    while t1 is not None and t1.info!=None:
+        t1 = t1.next
+
+    if t1 is not None:
+        print(f"Value {value} is not found")
+        return
+
+    t1.prev.next = t1.next
+    if t1.next is not None:
+        t1.next.prev = t1.prev
+
+
+# Full code for Double linked list
+class node:
+    def __init__(self,info=None):
+        self.info = info
+        self.next = None
+        self.prev = None
+class doublelinkedlist:
+    def __init__(self,head=None):
+        self.head = head
+    def is_big(self,value):
+        temp = node(value)
+        if self.head is None:
+            self.head = temp
+            return
+        else:
+            temp.next = self.head
+            self.head.prev = temp
+            self.head = temp
+    def is_mid(self,value,x):
+        if self.head is None:
+            print("List is empty")
+            return
+        t1 = self.head 
+        while t1 is not None and t1.info!=x:
+            t1 = t1.next
+        if t1 is None:
+            print(f"Node with these value {x} is not found")
+            return
+        temp = node(value)
+        temp.next = t1.next
+        if t1.next!=None:
+            t1.next.prev = temp
+        t1.next = temp
+        temp.prev = t1
+    def is_end(self,value):
+        temp = node(value)
+        if self.head is None:
+            self.head = temp
+            return
+        else:
+            t1 = self.head
+            while t1.next!=None:
+                t1 = t1.next
+            t1.next = temp
+            temp.prev = t1
+    def deletell(self,value):
+        if self.head is None:
+            print("List is empty")
+            return 
+        if self.head.info == value:
+            self.head = self.head.next
+            if self.head is None:
+                self.head.prev = None
+            return
+        t1 = self.head
+        while t1 is not None and t1.info!=None:
+            t1 = t1.next
+        if t1 is None:
+            print(f"Node with these value {value} is not found")
+            return
+        t1.prev.next = t1.next
+        if t1.next is not None:
+            t1.next.prev = t1.prev
+    def printll(self):
+        t1 = self.head
+        while t1!=None:
+            print(t1.info)
+            t1 = t1.next
+a = doublelinkedlist()
+a.is_big(10)
+a.is_mid(20,10)
+a.is_end(30)
+a.printll()
+"""Output = 10
+            20
+            30 """
